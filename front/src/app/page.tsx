6a8ee9a953144bec94b719c4227ebf7be31a1f2a
Shreyansh Pachouri <pachourishreyansh@gmail.com>
2026-09-15 19:42:08 +0530

    frontend part 1

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,13 +1,17 @@
-import FeatureSection from "@/components/base/FeatureSection";
-import Footer from "@/components/base/Footer";
-import HeroSection from "@/components/base/HeroSection";
-import Navbar from "@/components/base/Navbar";
-import UserReviews from "@/components/base/UserReviews";
+import { getServerSession } from "next-auth";
+import FeatureSection from "../components/base/FeatureSection";
+import Footer from "../components/base/Footer";
+import HeroSection from "../components/base/HeroSection";
+import Navbar from "../components/base/Navbar";
+import UserReviews from "../components/base/UserReviews";
+import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
 
-export default function Home(){
+export default async function Home(){
+  const session: CustomSession | null = await getServerSession(authOptions)
+
   return(
      <div className="min-h-screen flex flex-col ">
-      <Navbar />
+      <Navbar user = {session?.user} />
 
       <HeroSection />
 
