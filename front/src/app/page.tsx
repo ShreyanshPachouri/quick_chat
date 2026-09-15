e1e118de26633c9a34e5e0eb7e6a283910cc82af
Shreyansh Pachouri <pachourishreyansh@gmail.com>
2026-09-15 17:49:28 +0530

    basic landing page done

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,5 +1,21 @@
+import FeatureSection from "@/components/base/FeatureSection";
+import Footer from "@/components/base/Footer";
+import HeroSection from "@/components/base/HeroSection";
+import Navbar from "@/components/base/Navbar";
+import UserReviews from "@/components/base/UserReviews";
+
 export default function Home(){
   return(
-    <h1>Hello</h1>
+     <div className="min-h-screen flex flex-col ">
+      <Navbar />
+
+      <HeroSection />
+
+      <FeatureSection />
+
+      <UserReviews />
+
+      <Footer />
+    </div>
   )
 }
